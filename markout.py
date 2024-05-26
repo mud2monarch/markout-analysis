bef4b5d04193a2be5e7a49ecec635007cee0b6f5
Zach Wong <zach.wong@uniswap.org>
2024-05-26 13:59:36 -0400

    some bug fixes, seems to be working.

--- markout.py
+++ markout.py
@@ -6,8 +6,8 @@ def loadSwaps(
         dt_format: str = '%Y-%m-%d %H:%M:%S %Z'
 ) -> pl.DataFrame:
     """
-    @param filepath: pass the filepath as a relative or absolute path, including the extension.
-    @dt_format: you can pass a custom dt format. This uses the default from Allium.
+    @param filepath: str // pass the filepath as a relative or absolute path, including the extension.
+    @param dt_format: str // you can pass a custom dt format. The default is Allium's timestamp formatting.
 
     Notice: table should follow the schema described in all_swaps.sql
     """
@@ -38,7 +38,7 @@ def addPrice(
     Zach: I don't think this is possible actually because the problem is that you need to use native python dtypes.
     but maybe? not a priority.
     """
-    sqrtPrice_list = data.select(pl.col("sqrt_price_x96")).to_series().to_list()
+    sqrtPrice_list = data.select(pl.col('sqrtPriceX96')).to_series().to_list()
     price_list = [(int(i)/(2 ** 96))**2 for i in sqrtPrice_list]
     
     df = data.with_columns(pool_price = pl.Series(values=price_list, dtype=pl.Float64))
@@ -47,24 +47,31 @@ def addPrice(
 
 def construct_markout (
         data: pl.DataFrame,
+        timedelta: str = '5m'
 ) -> pl.DataFrame:
-    
+    """
+    @param data: pl.DataFrame // your Polars dataframe from loadSwaps()
+    @param timedelta: str // the markout timedelta you'd like to use, formatted per the documentation in `polars.Expr.dt.offset_by`
+
+    Function that adds all markout columns (execution_price, markout_price, direction, and volume) per-row.
+    """
+
     df = addPrice(data)
 
     df = (
         df.join_asof(
             other = df.select(
-                pl.col('block_timestamp').dt.offset_by('-5m').alias('timestamp_minus_five_min'),
+                pl.col('block_timestamp').dt.offset_by(f'-{timedelta}').alias('timestamp_minus_five_min'),
                 pl.col('pool_price'),
                 pl.col('address')
             ),
-            left_on='timestamp',
-            right_on='timestamp_plus_five_min',
+            left_on='block_timestamp',
+            right_on='timestamp_minus_five_min',
             by='address',
             strategy='backward'
         ).rename({'pool_price_right': 'markout_price'})
         .with_columns(
-            execution_price = ((10 ** (pl.col('token1_decimals')-pl.col('TOKEN0_DECIMALS')))
+            execution_price = ((10 ** (pl.col('token1_decimals')-pl.col('token0_decimals')))
                                 /
                                 (pl.col('amount1')/pl.col('amount0'))),
             volume = pl
@@ -78,92 +85,5 @@ def construct_markout (
             markout = (pl.col('direction') * pl.col('volume') * ((pl.col('markout_price') - pl.col('execution_price'))))
         )
     )
-
-    return df
-# def construct_markout(
-#         data: pl.DataFrame,
-#         IS_WETH_TOKEN0: bool,
-#         TOKEN0_DECIMALS: int,
-#         TOKEN1_DECIMALS: int
-# ) -> pl.DataFrame:
-
-#     sqrtprice_list = data.select(pl.col("sqrtPriceX96")).to_series().to_list()
-#     med_price = [(int(i)/(2 ** 96))**2 for i in sqrtprice_list]
-
-#     df=(
-#         data.select(
-#             'block_timestamp',
-#             'block_number',
-#             'amount0',
-#             'amount1',
-#             'sqrtPriceX96',
-#             'liquidity',
-#             'as_of'
-#         )
-#         .sort(
-#             'as_of'
-#         ).with_columns(
-#             [
-#                 pl.when(pl.col("amount1") < 0)
-#                 .then(-1)
-#                 .otherwise(1).alias('direction'),
-
-#                 ((10**(TOKEN1_DECIMALS-TOKEN0_DECIMALS))/(pl.col('amount1')/pl.col('amount0'))).alias('execution_px'),
-#                 # think i need this really cursed construction because i have been encountering an integer overflow from the following line. from grace.
-#                 # ((10**(TOKEN1_DECIMALS-TOKEN0_DECIMALS))/((pl.col('sqrtPriceX96').cast(pl.Float64)/(2 ** 96)) ** 2)).cast(pl.Float64).alias('markout_px')])
-                
-#                 pl.Series(values=med_price, dtype=pl.Float64).alias('unadj_markout_px')
-#             ]
-#         ).with_columns(
-#             [
-#                 ((10 ** (TOKEN1_DECIMALS-TOKEN0_DECIMALS)) / (pl.col('unadj_markout_px'))).alias('markout_px')
-#             ]
-#         ).with_columns(
-#             [
-#                 pl.col('markout_px')
-#                     .last() # last value over...
-#                     .over(pl.col('block_timestamp').dt.truncate('5m')) # 5 mins worth of 'block_timestamp'
-#                     .shift(-1).alias('markout_px_5m'),
-#                 pl.when(
-#                     pl.col('amount1') < 0,
-#                     ~IS_WETH_TOKEN0
-#                 ).then(
-#                     pl.col('amount1').abs()/ 10 ** TOKEN1_DECIMALS
-#                 ).when(
-#                     pl.col('amount1') < 0,
-#                     IS_WETH_TOKEN0
-#                 ).then(
-#                     pl.col('amount1').abs() / 10**TOKEN1_DECIMALS / pl.col('execution_px')
-#                 ).when(
-#                     pl.col('amount0') < 0,
-#                     IS_WETH_TOKEN0
-#                 ).then(
-#                     pl.col('amount0').abs() / 10 ** TOKEN0_DECIMALS
-#                 ).when(
-#                     pl.col('amount0') < 0,
-#                     ~IS_WETH_TOKEN0
-#                 ).then(
-#                     pl.col('amount0').abs() / 10 ** TOKEN0_DECIMALS / pl.col('execution_px')
-#                 ).otherwise(
-#                     pl.lit(None)
-#                 ).alias('volume')
-#             ]
-#         ).with_columns(
-#             [
-#                 (pl.col('direction') * pl.col('volume') * ((pl.col('markout_px_5m') - pl.col('execution_px')))).alias('markout')
-#             ]
-#         )
-#     )
-
-#     return df
-
-def execute_markout(
-    markout_df = pl.DataFrame
-) -> dict:
-
-    df = markout_df.select(
-        pl.sum('volume').alias('total_volume'),
-        pl.sum('markout').alias('total_markout')
-    ).to_dict()
 
     return df
